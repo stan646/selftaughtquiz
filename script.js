a82f3e5bae8e31d372741d8e50b4d26b7b01a472
Stanley Kambona <70863585+stan646@users.noreply.github.com>
2025-09-17 22:09:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,191 +1,64 @@
-const htmlQuestions = document.querySelectorAll('.question');
-let numbers = 0;
-const submitBtn = document.getElementById('submit-btn');
+// get question from html page (elements in a form of query selector)
+const containerQuestions = document.querySelectorAll('.question');
 
+// get total number of questions from the containerQuestions by using next and prev
+let totalQuestions = 0;
 
-
-// show only one question
-function fun(index){
-    htmlQuestions.forEach(q => q.style.display = 'none');
-    htmlQuestions[index].style.display = 'block';
+// function that allow each question to be visible when it called
+function loopQuestion(Index){
+    containerQuestions.forEach(question => question.style.display = 'none'); //hide all questions
+    containerQuestions[Index].style.display = 'block'; //show each question from array
 }
 
-// start with first question
-// fun(numbers);
+// show first question
 
-document.getElementById('level-1').addEventListener('click', ()=>{
-    document.getElementById('container-1').style.display = 'block';
-    document.getElementById('next-btn').style.display = 'block';
-    document.getElementById('prev-btn').style.display = 'block';
-    document.getElementById('prev-btn').disabled = true;
-   
-});
 
-// when clicking Next
-document.getElementById('next-btn').addEventListener('click', function(){
-    if(numbers < htmlQuestions.length - 1){
-        numbers++;
-        fun(numbers)
-    }else{
-        this.style.display = 'none';
-        submitBtn.style.display = 'block';
-    }
-    document.getElementById('prev-btn').disabled = false;
-});
-
-// when clicking Prev
-document.getElementById('prev-btn').addEventListener('click', function(){
-    if (numbers > 0) {
-        numbers--;
-        fun(numbers);
-        submitBtn.style.display = 'none';
-         document.getElementById('next-btn').style.display = 'block';
-    } else {
-        if (numbers < htmlQuestions.length - 1) {
-            this.disabled = true;
-        }
-    }
-});
-
-//submit button
-
-document.getElementById('quizForm').addEventListener('submit', function(event){
-    const Marks = []
-    let questionMarks = 2;
-
-    event.preventDefault();//prevent web refresh
-
+// call the button to show the first question from the beginner level
+document.getElementById('level-1').addEventListener('click', function(){
+    //if the total number 
     try {
-        // questions element 
-        const question1 = document.querySelector('input[name="q1"]:checked');
-        const question2 = document.querySelector('input[name="q2"]:checked');
-        const question3 = document.querySelector('input[name="q3"]:checked');
-        const question4 = document.querySelector('input[name="q4"]:checked');
-        const question5 = document.querySelector('input[name="q5"]:checked');
-        const question6 = document.querySelector('input[name="q6"]:checked');
-        const question7 = document.querySelector('input[name="q7"]:checked');
-        const question8 = document.querySelector('input[name="q8"]:checked');
-        const question9 = document.querySelector('input[name="q9"]:checked');
-        const question10 = document.querySelector('input[name="q10"]:checked');
-
-        // 
-
-        const container1 = document.getElementById('c-1');
-        const container2 = document.getElementById('c-2');
-        const container3 = document.getElementById('c-3');
-        const container4 = document.getElementById('c-4');
-        const container5 = document.getElementById('c-5');
-        const container6 = document.getElementById('c-6');
-        const container7 = document.getElementById('c-7');
-        const container8 = document.getElementById('c-8');
-        const container9 = document.getElementById('c-9');
-        const container10 = document.getElementById('c-10');
-
-        // validate user answers if is true or false
-
-        if (question1.value === 'a') {
-            Marks.push(questionMarks);
-            container1.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container1.innerHTML = `<div class="alert alert-danger alert-dismissiblew-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question2.value === 'b') {
-            Marks.push(questionMarks);
-            container2.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container2.innerHTML = `<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question3.value === 'a') {
-            Marks.push(questionMarks);
-            container3.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-        }else{
-           container3.innerHTML = `<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question4.value === 'b') {
-            Marks.push(questionMarks);
-            container4.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container4.innerHTML = `<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question5.value === 'b') {
-            Marks.push(questionMarks);
-            container5.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container5.innerHTML =`<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question6.value === 'a') {
-            Marks.push(questionMarks);
-            container6.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container6.innerHTML =`<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question7.value === 'b') {
-            Marks.push(questionMarks);
-            container7.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container7.innerHTML =`<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question8.value === 'b') {
-            Marks.push(questionMarks);
-            container8.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-           container8.innerHTML =  `<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question9.value === 'b') {
-            Marks.push(questionMarks);
-            container9.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-            
-        }else{
-            container9.innerHTML = `<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect</span> </div>`;
-            
-        }
-        if (question10.value === 'b') {
-            Marks.push(questionMarks);
-            container10.innerHTML = 
-            `<div class="alert alert-success alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Correct! ${questionMarks} Marks.</span> </div>`;
-        }else{
-           container10.innerHTML =  `<div class="alert alert-danger alert-dismissible w-75 w-sm-100 w-md-25"> <button class="btn-close" data-bs-dismiss="alert"></button><span>Incorrect!</span></div>`;
-            
-        }
-
-        // calclulate total mark
-        const totalMark = Marks.reduce(markscalculate);
-        console.log(totalMark);
-        
-
-        function markscalculate(valueA, valueB){
-            return valueA + valueB;
-        }
-
-        
+        loopQuestion(totalQuestions);
+        document.getElementById('next-btn').style.display = 'block';
+        document.getElementById('prev-btn').style.display = 'block';
+        document.getElementById('prev-btn').disabled = true;
+        this.disabled = true;
     } catch (error) {
-        console.log('something went wrong' + error.message);
+        console.log('Something is not okay !' + error.message);
     }
+});
 
+// next button
+document.getElementById('next-btn').addEventListener('click', function(){
+    try {
+        // if totalNumber of question is less than 1 increase quection.
+        if (totalQuestions < containerQuestions.length -1) {
+            totalQuestions++; //increase question by 1
+            loopQuestion(totalQuestions);//show the current question
+        }
 
-})
+        document.getElementById('prev-btn').disabled = false;
+    } catch (error) {
+        console.log('Error occur in ' + error.message);
+        
+    }
+    
+});
 
+// prev button
+document.getElementById('prev-btn').addEventListener('click', function(){
+    try {
+        // if totalNumber of question is great than 1 increase quection.
+        if (totalQuestions > 0) {
+            totalQuestions--; //decrease question by 1
+            loopQuestion(totalQuestions);//show the current question
+        }else{
+            if (totalQuestions < containerQuestions.length -1 ) {
+                this.disabled = true;
+            }
+        }
+    } catch (error) {
+        console.log('Error occur in ' + error.message);
+        
+    }
+    
+});
